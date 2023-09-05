5b6dccf0282d772643eda6d536637aae4bf85f07
An Nguyen <an6122003@gmail.com>
2023-09-05 13:38:32 +0700

    added leaderboard scene and dummy data

--- PC Invader/LeaderboardScene.swift
+++ PC Invader/LeaderboardScene.swift
@@ -9,21 +9,38 @@ import Foundation
 import SpriteKit
 
 class LeaderboardScene: SKScene{
+    var currentPage = 0
+    let recordsPerPage = 10
+    var totalPages: Int {
+        return (leaderboardData.count) / recordsPerPage + 1 // +1 to account for 1 excess page, ex: 21/2 = 2 pages + 1 page
+    }
     var backButton: SKSpriteNode!
+    var nextButton: SKSpriteNode!
+    var prevButton: SKSpriteNode!
     var leaderboardFrame: SKSpriteNode!
-    let leaderboardData: [String: Int] = [
-            "Player 1": 5000,
-            "Player 2": 7000,
-            "Player 3": 3000,
-            "Player 4": 5000,
-            "Player 5": 7000,
-            "Player 6": 3000,
-            "Player 7": 5000,
-            "Player 8": 7000,
-            "Player 9": 3000,
-        ]
+    var leaderboardData: [String: Int] = [
+        "You": UserDefaults.standard.integer(forKey: "highScore"),
+        "Tom": 500,
+        "An": 700,
+        "Bao": 300,
+        "Xiaomi": 500,
+        "Shelby": 700,
+        "Anh": 300,
+        "Player 7": 500,
+        "Player 8": 700,
+        "Player 9": 300,
+        "Player 10": 500,
+        "Player 11": 700,
+        "Player 12": 300,
+    ]
     
     override func didMove(to view: SKView) {
+        for i in 1...50 {
+            let randomName = "Player \(i + 12)"
+            let randomScore = Int.random(in: 1...1000)
+            leaderboardData[randomName] = randomScore
+        }
+        
         let background = SKSpriteNode(imageNamed: "background")
         
         background.position = CGPoint(x: self.size.width/2
@@ -45,44 +62,111 @@ class LeaderboardScene: SKScene{
         
         backButton = SKSpriteNode(imageNamed: "map-back-button")
         backButton.position = CGPoint(x: self.size.width * 0.25, y: self.size.height * 0.95)
-        backButton.zPosition = 5
+        backButton.zPosition = 3
         backButton.setScale(0.5)
         
+        // buttons for navigation of the leaderboard
+        nextButton = SKSpriteNode(imageNamed: "shop-forward-button")
+        nextButton.position = CGPoint(x: leaderboardFrame.position.x + 350, y: leaderboardFrame.position.y - 700)
+        nextButton.zPosition = 3
+        nextButton.setScale(0.7)
+        
+        prevButton = SKSpriteNode(imageNamed: "shop-backward-button")
+        prevButton.position = CGPoint(x: leaderboardFrame.position.x - 350, y: leaderboardFrame.position.y - 700)
+        prevButton.zPosition = 3
+        prevButton.setScale(0.7)
+        
+        let sortedLeaderboard = leaderboardData.sorted { $0.value > $1.value } // sort by descending order
+        var playerRank = -1
+
+        for (index, (playerName, _)) in sortedLeaderboard.enumerated() {
+            if playerName == "You" {
+                playerRank = index + 1
+                break // Exit the loop as the player's rank is found
+            }
+        }
+
+        // Player current rank label
+        let playerRankLabel = SKLabelNode(fontNamed: "ethnocentric")
+        playerRankLabel.fontSize = 45
+        playerRankLabel.position = CGPoint(x: leaderboardFrame.position.x, y: leaderboardFrame.position.y - 700)
+        if playerRank != -1 {
+            playerRankLabel.text = "Your Rank: \(playerRank)"
+        } else {
+            playerRankLabel.text = "Your Rank: N/A"
+        }
+        playerRankLabel.zPosition = 3
+        
+        self.addChild(playerRankLabel)
         self.addChild(background)
         self.addChild(headerLogo)
         self.addChild(leaderboardFrame)
         self.addChild(backButton)
-        displayLeaderboard()
+        self.addChild(nextButton)
+        self.addChild(prevButton)
+        displayLeaderboard(page: currentPage)
     }
     
-    func displayLeaderboard() {
-            // Sort the leaderboard data by high scores in descending order.
-            let sortedLeaderboard = leaderboardData.sorted { $0.value > $1.value }
-            
-            var initialPosition = self.leaderboardFrame!.position.y + 650
-            var yOffset: CGFloat = 300  // Adjust this value based on your design.
-            
-            // Iterate through the sorted data and create labels for player names and high scores.
-            for (rank, (playerName, highScore)) in sortedLeaderboard.enumerated() {
-                let nameLabel = SKLabelNode(fontNamed: "ethnocentric")
-                nameLabel.text = "\(rank + 1). \(playerName)"
-                nameLabel.fontSize = 50
-                nameLabel.horizontalAlignmentMode = .left
-                nameLabel.position = CGPoint(x: self.size.width / 2 - 325, y: initialPosition - yOffset)
-                nameLabel.zPosition = 3
-                self.addChild(nameLabel)
-                
-                let scoreLabel = SKLabelNode(fontNamed: "ethnocentric")
-                scoreLabel.text = "\(highScore)"
-                scoreLabel.fontSize = 50
-                scoreLabel.horizontalAlignmentMode = .right
-                scoreLabel.position = CGPoint(x: self.size.width / 2 + 335, y: initialPosition - yOffset)
-                scoreLabel.zPosition = 3
-                self.addChild(scoreLabel)
-                
-                yOffset += 100  // Adjust this value to control the vertical spacing between entries.
-            }
+    func displayLeaderboard(page: Int) {
+        
+        let startIndex = page * recordsPerPage // first record of each page
+        let endIndex = min(startIndex + recordsPerPage, leaderboardData.count) // In case their are less then 10 record per page
+        let sortedLeaderboard = leaderboardData.sorted { $0.value > $1.value } // sort by descending order
+
+        // Clear previous record
+        self.enumerateChildNodes(withName: "record"){
+            record, arg in
+            record.removeFromParent()
         }
+
+        // Display leaderboard for the current page
+        var initialPosition = self.leaderboardFrame!.position.y + 690
+        var yOffset: CGFloat = 300
+
+        for (index, (playerName, highScore)) in sortedLeaderboard[startIndex..<endIndex].enumerated() { //..< means from startIndex to less than endIndex
+            let rank = startIndex + (index + 1)
+            let nameLabel = SKLabelNode(fontNamed: "ethnocentric")
+            if playerName == "You"{
+                nameLabel.fontColor = .yellow
+            }
+            nameLabel.text = "\(rank). \(playerName)"
+            nameLabel.fontSize = 50
+            nameLabel.horizontalAlignmentMode = .left
+            nameLabel.position = CGPoint(x: self.size.width / 2 - 325, y: initialPosition - yOffset)
+            nameLabel.zPosition = 3
+            nameLabel.name = "record"
+            self.addChild(nameLabel)
+
+            let scoreLabel = SKLabelNode(fontNamed: "ethnocentric")
+            if playerName == "You"{
+                scoreLabel.fontColor = .yellow
+            }
+            scoreLabel.text = "\(highScore)"
+            scoreLabel.fontSize = 50
+            scoreLabel.horizontalAlignmentMode = .right
+            scoreLabel.position = CGPoint(x: self.size.width / 2 + 335, y: initialPosition - yOffset)
+            scoreLabel.zPosition = 3
+            scoreLabel.name = "record"
+            self.addChild(scoreLabel)
+            
+            yOffset += 100
+        }
+    }
+    
+    
+    func nextPage() {
+        if currentPage < totalPages - 1 {
+            currentPage += 1
+            displayLeaderboard(page: currentPage)
+        }
+    }
+
+    func prevPage() {
+        if currentPage > 0 {
+            currentPage -= 1
+            displayLeaderboard(page: currentPage)
+        }
+    }
     
     func changeScene(sceneToMove: SKScene){
         sceneToMove.size = self.size
@@ -97,6 +181,14 @@ class LeaderboardScene: SKScene{
             
             if backButton.contains(location){
                 changeScene(sceneToMove: MainMenuScene(size: self.size))
+            }
+            
+            if nextButton.contains(location){
+                nextPage()
+            }
+            
+            if prevButton.contains(location){
+                prevPage()
             }
         }
     }
---
+++ PC Invader/test.swift
@@ -0,0 +1,20 @@
+//
+//  test.swift
+//  PC Invader
+//
+//  Created by An on 9/5/23.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct test_Previews: PreviewProvider {
+    static var previews: some View {
+        test()
+    }
+}
